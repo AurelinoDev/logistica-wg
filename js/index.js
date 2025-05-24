937e92f5c2803d064a807fea7b8ed9d0efb7216a
Igor-Aurelino <i.rufino@aluno.senai.br>
2025-05-23 23:16:40 -0300

    Index Finalizado e responsividade 80%

--- js/index.js
+++ js/index.js
@@ -1,20 +1,4 @@
-// // Index
-// let count = 1;
-// document.getElementById("radio1").checked = true;
 
-// setInterval(function(){
-//   nextImage();
-// }, 3000)
-
-// function nextImage(){
-//   count++;
-//   if(count>3){
-//     count = 1;
-//   }
-//   document.getElementById("radio"+count).checked = true;
-// }
-
-// Script automático para o slider
 let count = 1;
 document.getElementById("radio1").checked = true;
 
